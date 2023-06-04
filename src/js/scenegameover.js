1aa8cf08d66eb7645810f8d0037040118a50254d
Roel-H <roelhoogendoorn01369@gmail.com>
2023-06-04 20:06:31 +0200

    Working high score. updated version

--- src/js/scenegameover.js
+++ src/js/scenegameover.js
@@ -1,4 +1,4 @@
-import { Color, Label, Scene, Vector } from 'excalibur'
+import { Color, Font, Label, Scene, Vector } from 'excalibur'
 
 export class GameOver extends Scene {
     game
@@ -8,25 +8,30 @@ export class GameOver extends Scene {
 
     onInitialize(engine) {
         this.game = engine
-        this.highScore = parseFloat(localStorage.getItem('highScore')).toFixed(2) || 0
 
         this.highScoreLabel = new Label({
             pos: new Vector(100, 200),
             width: 200,
             height: 40,
-            color: Color.White
-        })
+            color: Color.White,
+            font: new Font({
+                size: 30
+            })
+        });
         this.add(this.highScoreLabel)
     }
 
     onActivate(ctx) {
         console.log('The scene Game Over has started!')
 
+        this.highScore = parseFloat(localStorage.getItem('highScore')) || 0
+
         if (ctx.data) {
             this.currentTime = parseFloat(ctx.data.time.toFixed(2))
 
             if (this.currentTime > this.highScore) {
-                localStorage.setItem('highScore', this.currentTime)
+                this.highScore = this.currentTime
+                localStorage.setItem('highScore', this.highScore.toFixed(2))
                 this.showHighScoreText(true)
             } else {
                 this.showHighScoreText(false)
@@ -38,7 +43,10 @@ export class GameOver extends Scene {
             width: 100,
             height: 40,
             text: 'Start',
-            color: Color.White
+            color: Color.White,
+            font: new Font({
+                size: 30
+            })
         })
         startButton.on('pointerup', () => {
             this.game.goToScene('start')
@@ -48,9 +56,9 @@ export class GameOver extends Scene {
 
     showHighScoreText(isNewHighScore) {
         if (isNewHighScore) {
-            this.highScoreLabel.text = `You got a new High Score: ${this.highScore}`
+            this.highScoreLabel.text = `You got a new High Score: ${this.highScore.toFixed(2)}`
         } else {
-            this.highScoreLabel.text = `Time: ${this.currentTime} High Score: ${this.highScore}`
+            this.highScoreLabel.text = `Time: ${this.currentTime.toFixed(2)} | High Score: ${this.highScore.toFixed(2)}`
         }
     }
 }
